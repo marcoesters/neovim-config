66090cb064907b8dea8c1759cd4cb2ea0a318892
Marco Esters <marco.esters.phd@gmail.com>
2024-10-28 20:56:20 -0700

    Add color schemes

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        opts = {
+            keywordStyle = {
+                italic = false,
+            },
+            overrides = function(_, opts)
+                return {
+                    ["@variable.builtin"] = { italic = false },
+                }
+            end,
+        },
+    },
+    {
+        "sho-87/kanagawa-paper.nvim",
+        priority = 1000,
+        opts = {
+            overrides = function(_, opts)
+                return {
+                    ["@variable.builtin"] = { italic = false },
+                }
+            end,
+        },
+    },
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000,
+    },
+}
