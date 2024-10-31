bde9f67b73393248af638642bf8a5bad47b5d7b8
Marco Esters <mesters@anaconda.com>
2024-10-31 13:54:29 -0700

    Use vim regex highlighting for YAML files

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -4,6 +4,7 @@ return {
     highlight = {
         enable = true,
         use_languagetree = true,
+        additional_vim_regex_highlighting = { "yaml" },
     },
 
     incremental_selection = {
