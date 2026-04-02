4e2dde1e70436d8695f5801357367b6ad3639a4d
Marco Esters <mesters@anaconda.com>
2026-04-02 16:23:41 -0700

    Add neo-tree

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            filesystem = {
+                hijack_netrw_behavior = "open_current",
+                filtered_items = {
+                    visible = false,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_by_name = {
+                        '.git',
+                        '__pycache__',
+                    },
+                    never_show = {},
+                },
+            },
+            window = {
+                mappings = {
+                    -- Disable C-f so it doesn't conflict with tmux-sessionizer shortcut
+                    ["<C-f>"] = false
+                }
+            },
+        })
+        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal bottom toggle=true<CR>')
+    end
+}
