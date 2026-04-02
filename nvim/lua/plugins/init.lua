cc027b54e2f76a1aaf2e718e180705cb07739450
Marco Esters <mesters@anaconda.com>
2026-04-02 14:37:07 -0700

    Revert "Disable lazy loading for treesitter (incompatible with latest version)"
    This reverts commit ed21c09c78f165bf760250a1d640a59971eb7a40.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -9,10 +9,14 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     build = ":TSUpdate",
     opts = function()
       return require "configs.treesitter"
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
     end,
   },
   {
